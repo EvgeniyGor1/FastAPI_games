93753bf497c3a4d6165f54c10b1671b2a8d50e55
Evgeniy <gorlow.jenya@gmail.com>
2025-02-08 20:04:09 +0300

    create config.py

---
+++ src/config.py
@@ -0,0 +1,37 @@
+from dotenv import load_dotenv
+import os
+
+from pydantic import computed_field
+from pydantic_core import MultiHostUrl
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+load_dotenv()
+
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
+
+
+class Config(BaseSettings):
+    model_config = SettingsConfigDict(env_file="./.env")
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    POSTGRES_DB: str
+
+    @computed_field
+    def DATABASE_URI(self) -> str:
+        return str(MultiHostUrl.build(
+            scheme="postgresql+asyncpg",
+            username=self.DB_USER,
+            password=self.DB_PASS,
+            host=self.DB_HOST,
+            port=self.DB_PORT,
+            path=self.POSTGRES_DB
+        ))
+
+
+settings = Config()
