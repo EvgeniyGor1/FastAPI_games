52542f1a5df56881ca9c291cdbaff56bbbe978d8
Evgeniy <gorlow.jenya@gmail.com>
2025-02-12 19:38:54 +0300

    database fixes

--- src/database.py
+++ src/database.py
@@ -5,28 +5,22 @@ from sqlalchemy.ext.asyncio import (
     async_sessionmaker,
     create_async_engine,
     AsyncSession,
-    async_scoped_session,
 )
 
 from src.config import settings
 
 
 engine = create_async_engine(settings.DATABASE_URI)
-async_session_maker = async_sessionmaker(engine, xpire_on_commit=False, echo=True)
-
-
-def get_scoped_session() -> async_scoped_session[AsyncSession]:
-    session = async_scoped_session(
-        session_factory=async_session_maker,
-        scopefunc=current_task,
-    )
-    return session
+async_session_maker = async_sessionmaker(
+    engine,
+    xpire_on_commit=False,
+    echo=True,
+)
 
 
 async def session_dependency() -> AsyncSession:
-    session = get_scoped_session()
-    yield session
-    await session.remove()
+    async with async_session_maker() as session:
+        yield session
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
